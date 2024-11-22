ceecf5d9841e845c2045d67b7a3fc6cc665ffb7a
AnF <anfwork007@gmail.com>
2024-11-23 00:15:17 +0300

    test passed

--- src/test/java/ru/netology/card/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/card/delivery/CardDeliveryTest.java
@@ -1,18 +1,36 @@
 package ru.netology.card.delivery;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
     @Test
-    public void NewTest() throws InterruptedException {
+    public void CardOrderTest() throws InterruptedException {
         open("http://localhost:9999");
         SelenideElement form = $(".form_size_m");
-        
-        Thread.sleep(5000);
+        form.$("[placeholder='Город']").setValue("Санкт-Петербург");
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.COMMAND+"A");
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        form.$("[placeholder='Дата встречи']").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        form.$("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        form.$("[name='phone']").setValue("+79111112233");
+        form.$(".checkbox").click();
+        form.$(".button").click();
+        form.$(".button").shouldBe(visible,Duration.ofSeconds(15));
+
+        Thread.sleep(15000);
     }
 }
